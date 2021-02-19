825fe4d130de841cd14d1b0c75788d080aefad67
Alessandro Hultman <alessandrosisfontes@hotmail.com>
2021-02-19 13:22:48 +0100

    Add implementation in Socket.hs

--- src/Socket.hs
+++ src/Socket.hs
@@ -8,17 +8,26 @@ module Socket
 where
 
 import           Data.ByteString
+import qualified Network.Socket.ByteString as BS
 import           Network.Socket
 
+sockType :: SocketType
+sockType = Stream
 
-initSocket :: SocketType -> IO Socket
-initSocket = undefined
+convertIP :: String -> HostAddress
+convertIP publicIP = undefined
+
+initSocket :: SocketType -> IO ()
+initSocket sockType = withSocketsDo $ do
+  socket <- socket AF_INET sockType 0
+  setSocketOption socket ReuseAddr 1
+  bind socket (SockAddrInet 5050 convertIP (someIP))
 
 sendBytes :: Socket -> ByteString -> IO ()
-sendBytes = undefined
+sendBytes socket = BS.sendAll socket someByteString
 
 receiveBytes :: Socket -> IO ByteString
-receiveBytes = undefined
+receiveBytes socket = BS.recv socket 2
 
 closeSocket :: Socket -> IO ()
-closeSocket = undefined
+closeSocket = close
