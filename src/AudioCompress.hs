3c457db501b044e77cb96c57198a93990fe0232c
karlerikgustavpettersson <karl.pettersson.6487@student.uu.se>
2021-02-21 15:10:31 +0100

    Add compression functionality to bytestrings

--- src/AudioCompress.hs
+++ src/AudioCompress.hs
@@ -1,18 +1,39 @@
 -- Functions to compress and decompress raw audio. The compressed audio is stored in a ByteString.
-module AudioCompress (compress, decompress) where
+module AudioCompress (compressAudio, decompressAudio) where
 
 import           Data.ByteString
+import           Data.Maybe
+import           Data.Vector.Storable.ByteString as V
 import           Samples
+import qualified Codec.Compression.LZ4 as LZ
 
+{- compressBytestring bs
+   Compresses a bytestring using LZ4 encoding.
+   RETURNS: compressed bytestring of bs
+   EXAMPLES: TODO
+-}
+
+compressBytestring :: ByteString -> ByteString
+compressBytestring bs = fromJust (LZ.compress bs)
+
+{- decompressBytestring bs
+   Decompresses a bytestring using LZ4 decoding
+   PRE: bs must be the result of calling compressBytestring bs.
+   RETURNS: TODO
+   EXAMPLES: TODO
+-}
+
+decompressBytestring :: ByteString -> ByteString
+decompressBytestring bs = fromJust (LZ.decompress bs)
 {- compress rawAudio
  - Compress a raw stream of audio.
  -}
-compress :: SampleList -> ByteString
-compress = undefined
+compressAudio :: SampleList -> ByteString
+compressAudio = undefined
 
 {- decompress compressedBytes
  - Decompress some compressed bytes to raw audio.
  - PRE: decompressedBytes must be the result of a call to compress for any raw audio.
  -}
-decompress :: ByteString -> SampleList
-decompress = undefined
+decompressAudio :: ByteString -> SampleList
+decompressAudio = undefined
