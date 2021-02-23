959ee7329044f6d81e97ab6a6c337061365ecf95
Tage Johansson <tajo9185@uu.student.se>
2021-02-23 11:51:27 +0100

    Improve crypto module and add crypto tests.

--- src/Crypto.hs
+++ src/Crypto.hs
@@ -1,13 +1,18 @@
 module Crypto
-  ( Keypair
-  , ServerHandShake
-  , ClientHandShake
-  , SecretConnection
+  ( PublicKey
+  , Code
+  , ServerSecretKey
+  , ClientSecretKey
+  , ServerHandshake
+  , ClientHandshake
+  , Encrypter
+  , Decrypter
   , Crypto
   , initCrypto
-  , newKeypair
-  , serverHandShake
-  , clientHandShake
+  , newServerHandshake
+  , newClientHandshake
+  , serverHandshakeToByteString
+  , serverHandshakeFromByteString
   , createServer
   , createClient
   , encrypt
@@ -16,19 +21,39 @@ module Crypto
 where
 
 import           Crypto.Saltine                 ( sodiumInit )
-import           Crypto.Saltine.Class           ( nudge )
-import           Crypto.Saltine.Core.Box hiding ( Keypair
-                                                , newKeypair
-                                                )
-import qualified Crypto.Saltine.Core.Box       as SaltineBox
+import qualified Crypto.Saltine.Class          as Saltine.Class
+import           Crypto.Saltine.Core.Box
+import           Data.Binary
 import           Data.ByteString                ( ByteString )
+import           Data.ByteString.Lazy           ( fromStrict
+                                                , toStrict
+                                                )
+import           Data.Word                      ( Word64 )
 
-data Keypair = Keypair SecretKey PublicKey
+type Code = Word64
 
-data ServerHandShake = ServerHandShake PublicKey Nonce
-data ClientHandShake = ClientHandShake PublicKey
+newtype ServerSecretKey = ServerSecretKey SecretKey
 
-data SecretConnection = SecretConnection CombinedKey Nonce Nonce
+newtype ClientSecretKey = ClientSecretKey SecretKey
+
+data ServerHandshake = ServerHandshake PublicKey Nonce Code
+data ClientHandshake = ClientHandshake PublicKey Code
+
+instance Binary ServerHandshake where
+  put (ServerHandshake pubKey nonce code) = do
+    put $ Saltine.Class.encode pubKey
+    put $ Saltine.Class.encode nonce
+    put code
+
+  get = do
+    Just pubKey <- Saltine.Class.decode <$> get
+    Just nonce  <- Saltine.Class.decode <$> get
+    code        <- get
+    return $ ServerHandshake pubKey nonce code
+
+data Encrypter = Encrypter CombinedKey Nonce
+
+data Decrypter = Decrypter CombinedKey Nonce
 
 data Crypto = Crypto
 
@@ -37,51 +62,72 @@ initCrypto = do
   sodiumInit
   return Crypto
 
-newKeypair :: Crypto -> IO Keypair
-newKeypair _ = do
-  (secKey, pubKey) <- SaltineBox.newKeypair
-  return $ Keypair secKey pubKey
+newServerHandshake :: Crypto -> Code -> IO (ServerSecretKey, ServerHandshake)
+newServerHandshake _ code = do
+  (secKey, pubKey) <- newKeypair
+  nonce            <- newNonce
+  return (ServerSecretKey secKey, ServerHandshake pubKey nonce code)
 
-serverHandShake :: Keypair -> IO ServerHandShake
-serverHandShake (Keypair _ pubKey) = do
-  nonce <- newNonce
-  return $ ServerHandShake pubKey nonce
+newClientHandshake :: Crypto -> Code -> IO (ClientSecretKey, ClientHandshake)
+newClientHandshake _ code = do
+  (secKey, pubKey) <- newKeypair
+  return (ClientSecretKey secKey, ClientHandshake pubKey code)
 
-clientHandShake :: Keypair -> IO ClientHandShake
-clientHandShake (Keypair _ pubKey) = return $ ClientHandShake pubKey
+serverHandshakeToByteString :: ServerHandshake -> ByteString
+serverHandshakeToByteString = toStrict . encode
 
+serverHandshakeFromByteString :: ByteString -> ServerHandshake
+serverHandshakeFromByteString = decode . fromStrict
 
 createServer
-  :: Keypair -> ServerHandShake -> ClientHandShake -> SecretConnection
-createServer keypair@(Keypair serverSecKey _) (ServerHandShake _ encryptNonce) (ClientHandShake clientPubKey)
-  = let combinedKey  = beforeNM serverSecKey clientPubKey
-        decryptNonce = nudge encryptNonce
-    in  SecretConnection combinedKey encryptNonce decryptNonce
+  :: ServerSecretKey
+  -> ServerHandshake
+  -> ClientHandshake
+  -> Maybe (Encrypter, Decrypter)
+createServer (ServerSecretKey serverSecKey) (ServerHandshake _ encryptNonce serverCode) (ClientHandshake clientPubKey clientCode)
+  = let
+      combinedKey  = beforeNM serverSecKey clientPubKey
+      decryptNonce = Saltine.Class.nudge encryptNonce
+    in
+      if serverCode == clientCode
+        then
+          Just
+            ( Encrypter combinedKey encryptNonce
+            , Decrypter combinedKey decryptNonce
+            )
+        else Nothing
 
 createClient
-  :: Keypair -> ServerHandShake -> ClientHandShake -> SecretConnection
-createClient keypair@(Keypair clientSecKey _) (ServerHandShake serverPubKey decryptNonce) _
-  = let combinedKey  = beforeNM clientSecKey serverPubKey
-        encryptNonce = nudge decryptNonce
-    in  SecretConnection combinedKey encryptNonce decryptNonce
+  :: ClientSecretKey
+  -> ServerHandshake
+  -> ClientHandshake
+  -> Maybe (Encrypter, Decrypter)
+createClient (ClientSecretKey clientSecKey) (ServerHandshake serverPubKey decryptNonce serverCode) (ClientHandshake _ clientCode)
+  = let
+      combinedKey  = beforeNM clientSecKey serverPubKey
+      encryptNonce = Saltine.Class.nudge decryptNonce
+    in
+      if serverCode == clientCode
+        then
+          Just
+            ( Encrypter combinedKey encryptNonce
+            , Decrypter combinedKey decryptNonce
+            )
+        else Nothing
 
-encrypt :: SecretConnection -> ByteString -> (SecretConnection, ByteString)
-encrypt (SecretConnection combinedKey encryptNonce decryptNonce) message =
-  let
-    cyphertext      = boxAfterNM combinedKey encryptNonce message
-    newEncryptNonce = nudge $ nudge encryptNonce
-    newSecretConnection =
-      SecretConnection combinedKey newEncryptNonce decryptNonce
-  in
-    (newSecretConnection, cyphertext)
+encrypt :: Encrypter -> ByteString -> (Encrypter, ByteString)
+encrypt (Encrypter combinedKey encryptNonce) message =
+  let cyphertext      = boxAfterNM combinedKey encryptNonce message
+      newEncryptNonce = Saltine.Class.nudge $ Saltine.Class.nudge encryptNonce
+      newEncrypter    = Encrypter combinedKey newEncryptNonce
+  in  (newEncrypter, cyphertext)
 
-decrypt
-  :: SecretConnection -> ByteString -> (SecretConnection, Maybe ByteString)
-decrypt (SecretConnection combinedKey encryptNonce decryptNonce) cyphertext =
-  let
-    message         = boxOpenAfterNM combinedKey decryptNonce cyphertext
-    newDecryptNonce = nudge $ nudge decryptNonce
-    newSecretConnection =
-      SecretConnection combinedKey encryptNonce newDecryptNonce
-  in
-    (newSecretConnection, message)
+decrypt :: Decrypter -> ByteString -> Maybe (Decrypter, ByteString)
+decrypt (Decrypter combinedKey decryptNonce) cyphertext =
+  (\message ->
+      let newDecryptNonce =
+              Saltine.Class.nudge $ Saltine.Class.nudge decryptNonce
+          newDecrypter = Decrypter combinedKey newDecryptNonce
+      in  (newDecrypter, message)
+    )
+    <$> boxOpenAfterNM combinedKey decryptNonce cyphertext
