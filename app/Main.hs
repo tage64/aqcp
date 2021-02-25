4d52b0a59ed67669304e3fffc7b61efa0f51c8ba
Tage Johansson <tajo9185@uu.student.se>
2021-02-25 10:08:37 +0100

    Remove src/Lib.hs.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,4 @@
 module Main where
 
-import Lib
-
 main :: IO ()
-main = someFunc
+main = putStrLn "The program is not done yet. Ask the developers to code faster and grap a good cup of coffie while waiting."
